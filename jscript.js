260991a90a372e97d7cc19ed52c67d56ac512723
githmin <githmin21@gmail.com>
2022-06-07 15:36:42 +0530

    Added a loop to run the game 5 times

--- jscript.js
+++ jscript.js
@@ -2,24 +2,24 @@ console.log("Hello World! This is a Rock Paper Scissor Game!");
 
 let computerGuess
 let userMarks
+let userInput
 
 function computerPlay(){
     let randomNum = Math.floor(Math.random() * 3);
     console.log(randomNum);
     if(randomNum===0){
-        console.log("Rock");
+        console.log("Computer Guess is Rock");
         computerGuess = 0;
         return computerGuess;
     } else if (randomNum===1){
-        console.log("Paper");
+        console.log("Computer Guess is Paper");
         computerGuess = 1;
     } else {
-        console.log("Scissor")
+        console.log("Computer Guess is Scissor")
         computerGuess = 2;
     }
 }
 
-console.log(computerGuess)
 
 function comparingCode(){
     if (userInput==="rock" && computerGuess===0){
@@ -40,12 +40,19 @@ function comparingCode(){
         console.log("Computer Wins");
     } else if (userInput==="scissor" && computerGuess===1){
         console.log("User Wins");
+    }else {
+        console.log("Invalid");
     }
-    
 }
 
+function playerGuess() {
+    userInput = window.prompt("Is it Rock , Paper or Scissors?");
+    console.log("User Input is" ,userInput)
+    return userInput;
+}
 
-
-let userInput = window.prompt("Is it Rock , Paper or Scissors?");
-computerPlay()
-comparingCode()
+for (let i = 0; i < 5; i++){
+    playerGuess();
+    computerPlay();
+    comparingCode();
+}
