f6ba95be6dd3ac13d5ad2a424d499fd643f230e6
githmin <githmin21@gmail.com>
2022-06-07 13:58:04 +0530

    Random word selector

--- jscript.js
+++ jscript.js
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+console.log("Hello World! This is a Rock Paper Scissor Game!");
+
+
+
+
+function computerPlay(){
+    let randomNum = Math.floor(Math.random() * 3);
+    console.log(randomNum);
+    if(randomNum===0){
+        console.log("Rock");
+    } else if (randomNum===1){
+        console.log("Paper");
+    } else {
+        console.log("Scissor")
+    }
+}
+
+ computerPlay()
