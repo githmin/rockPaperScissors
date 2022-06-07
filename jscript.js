5117c3db9d2c5b5d3026f7fd1113439eb76eea3f
githmin <githmin21@gmail.com>
2022-06-07 15:02:51 +0530

    Added comparing code

--- jscript.js
+++ jscript.js
@@ -1,18 +1,51 @@
 console.log("Hello World! This is a Rock Paper Scissor Game!");
 
-
-
+let computerGuess
+let userMarks
 
 function computerPlay(){
     let randomNum = Math.floor(Math.random() * 3);
     console.log(randomNum);
     if(randomNum===0){
         console.log("Rock");
+        computerGuess = 0;
+        return computerGuess;
     } else if (randomNum===1){
         console.log("Paper");
+        computerGuess = 1;
     } else {
         console.log("Scissor")
+        computerGuess = 2;
     }
 }
 
- computerPlay()
+console.log(computerGuess)
+
+function comparingCode(){
+    if (userInput==="rock" && computerGuess===0){
+        console.log("Tie");
+    } else if (userInput==="paper" && computerGuess===1){
+        console.log("Tie");
+    } else if (userInput==="scissor" && computerGuess===2){
+        console.log("Tie");
+    } else if (userInput==="rock" && computerGuess===1){
+        console.log("Computer Wins");
+    } else if (userInput==="rock" && computerGuess===2){
+        console.log("User Wins");
+    } else if (userInput==="paper" && computerGuess===0){
+        console.log("User Wins");
+    } else if (userInput==="paper" && computerGuess===2){
+        console.log("Computer Wins");
+    } else if (userInput==="scissor" && computerGuess===0){
+        console.log("Computer Wins");
+    } else if (userInput==="scissor" && computerGuess===1){
+        console.log("User Wins");
+    }
+    
+}
+
+
+
+let userInput = window.prompt("Is it Rock , Paper or Scissors?");
+computerPlay()
+comparingCode()
