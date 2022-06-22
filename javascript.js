4126d2accbc5ecf43020b210f372d272859487fe
Githmin Jayawardhana <githmin21@gmail.com>
2022-06-22 16:00:53 +0530

    Cleaned up code

--- javascript.js
+++ javascript.js
@@ -1,19 +1,19 @@
 let userInput;
 let counter = 0;
 let counterM;
-let userMarks;
-let pcMarks;
+let userMarks = 0;
+let pcMarks = 0;
 let userTemp;
 let pcTemp;
-
+let clicks = 0 ;
 
 const rock = document.querySelector('#rock');
 rock.addEventListener('click', () => {
-    countCheck();
+    
     userInput = "rock";
     console.log('User input is rock');
     computerPlay();
-    counter = ++counter
+    counterM= ++counter
     console.log(counter, "-----");
     comparingCode();
 
@@ -21,33 +21,28 @@ rock.addEventListener('click', () => {
 
 const paper = document.querySelector('#paper');
 paper.addEventListener('click' , () => {
-    countCheck();
     userInput = "paper";
     console.log('User input is paper');
     computerPlay();
-    counter = ++counter
-    console.log(counter);
+    counterM = ++counter
+    console.log(counter, "-----");
     comparingCode();
 
 })
 
 const s = document.querySelector('#s');
 s.addEventListener('click' , () => {
-    countCheck();
     userInput = "scissor";
     console.log('User input is scissor');
     computerPlay();
-    counter = ++counter
-    console.log(counter);
+    counterM = ++counter
+    console.log(counter, "-----");
     comparingCode();
 })
 
 
-console.log(userInput);
-
 function computerPlay(){
     let randomNum = Math.floor(Math.random() * 3);
-    // console.log(randomNum);
     if(randomNum===0){
         console.log("Computer Guess is Rock");
         computerGuess = 0;
@@ -71,41 +66,39 @@ function comparingCode(){
     } else if (userInput==="scissor" && computerGuess===2){
         console.log("Tie");
     } else if (userInput==="rock" && computerGuess===1){
-        console.log("Computer Wins");
+        console.log("Computer Wins the round");
         pcMarks = pcMarks++;
     } else if (userInput==="rock" && computerGuess===2){
-        console.log("User Wins");
+        console.log("User Wins the round");
         userMarks = userMarks++;
     } else if (userInput==="paper" && computerGuess===0){
-        console.log("User Wins");
+        console.log("User Wins the round");
         userMarks = userMarks++;
     } else if (userInput==="paper" && computerGuess===2){
-        console.log("Computer Wins");
+        console.log("Computer Wins the round");
         pcMarks = pcMarks++;
     } else if (userInput==="scissor" && computerGuess===0){
-        console.log("Computer Wins");
+        console.log("Computer Wins the round");
         pcMarks = pcMarks++;
     } else if (userInput==="scissor" && computerGuess===1){
-        console.log("User Wins");
+        console.log("User Wins the round");
         userMarks = userMarks++;
     }else {
         console.log("Invalid");
     }
 }
 
-function countCheck(){
-    
-    if(counter = 5){
+rock.addEventListener('click', function () {
+	clicks = ++clicks;
+    console.log("Clicks " , clicks);
+});
 
-        if (userMarks < pcMarks) {
-            console.log("PC Wins bish")
-        }else {
-            console.log("User Wins bish")
-        }
+paper.addEventListener('click', function () {
+	clicks = ++clicks;
+    console.log("Clicks " , clicks);
+});
 
-    } else {
-    
-        console.log("counter is---------------------- "  ,counter);
-        
-    }
-}
+s.addEventListener('click', function () {
+	clicks = ++clicks;
+    console.log("Clicks " , clicks);
+});
